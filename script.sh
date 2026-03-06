fe803384480a0553f3b0ae40594d718419697574
Jason Freeberg <jason.freeberg@snowflake.com>
2026-03-06 12:46:14 -0800

    better query

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ npo_name=my_project
 snow stage create $stage_name
 snow stage copy --recursive --overwrite . @$stage_name
 snow notebook project create $npo_name --source @$stage_name/ --overwrite
-snow notebook project execute my_project --main-file=src/main.py
+# snow notebook project execute my_project --main-file=src/main.py
