f1851bd39a15ed02a1dfa898e661bfe6914c412c
Jason Freeberg <jason.freeberg@snowflake.com>
2026-03-31 15:55:37 -0700

    update

--- script.sh
+++ script.sh
@@ -4,4 +4,4 @@ npo_name=my_project2
 snow stage create $stage_name
 snow stage copy --recursive --overwrite . @$stage_name
 snow notebook project create $npo_name --source @$stage_name/ --overwrite
-snow notebook project execute $npo_name --main-file=src/udf_example.py
+snow notebook project execute $npo_name --main-file=src/main.py
