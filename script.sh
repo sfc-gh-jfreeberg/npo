0a17f52e3246029b46664e11a0dce741b02351d7
Jason Freeberg <jason.freeberg@snowflake.com>
2026-05-19 10:24:26 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ object_name=my_python_project
 
 # Deploy the project to Snowflake
 snow notebook project create $object_name \
-    --source . \  # Uploads the current directory as the project source 
+    --source . \ 
     --overwrite \
 
 # Execute the main.py file in the project
